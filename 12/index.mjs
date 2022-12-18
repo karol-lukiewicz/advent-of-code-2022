febcdbc6e1b2f59a1dd9c5148e8a9fef30cd6fee
Karol Łukiewicz <karol.lukiewicz@ocado.com>
2022-12-18 21:46:22 +0100

    feat: day 12

--- 12/index.mjs
+++ 12/index.mjs
@@ -1,50 +1,98 @@
 #!/usr/bin/env node
 import fs from "fs";
+import graphlib from "@dagrejs/graphlib";
 
-function day12(input) {
-    console.log("INPUT:");
-    console.log(input);
-    const grid = input.split("\n").map(line => line.split("").map(c => c.charCodeAt(0)));
+function parseMap(input) {
+    return input.split("\n").map(line => line.split("").map(c => c.charCodeAt(0)));
+}
 
-    const graph = {};
-    for (let h = 0; h < grid.length; h++) {
-        for (let w = 0; w < grid[h].length; w++) {
-            const nodeName = `${h},${w}`;
-            const elevation = grid[h][w];
-            if (elevation === "S".charCodeAt(0)) {
-                console.log(`Start: [${h},${w}]`);
+function nodeName(h, w) {
+    return `${h},${w}`;
+}
+
+function buildGraph(grid) {
+    const graph = new graphlib.Graph();
+
+    function registerConnection(startNode, startElevation, h, w) {
+        if (grid[h]) {
+            const adjacentNode = nodeName(h, w);
+            if (grid[h][w] <= startElevation + 1) {
+                graph.setEdge(startNode, adjacentNode);
             }
-            if (elevation === "E".charCodeAt(0)) {
-                console.log(`End: [${h},${w}]`);
-            }
-            const connectedNodes = {};
-            if (grid[h] && (grid[h][w - 1] === elevation || grid[h][w - 1] === elevation+1)) connectedNodes[`${h},${w - 1}`] = 1;
-            if (grid[h - 1] && (grid[h - 1][w] === elevation || grid[h - 1][w] === elevation+1)) connectedNodes[`${h - 1},${w}`] = 1;
-            if (grid[h] && (grid[h][w + 1] === elevation || grid[h][w + 1] === elevation+1)) connectedNodes[`${h},${w + 1}`] = 1;
-            if (grid[h + 1] && (grid[h + 1][w] === elevation || grid[h + 1][w] === elevation+1)) connectedNodes[`${h + 1},${w}`] = 1;
-            graph[nodeName] = connectedNodes;
         }
     }
-    console.log(graph);
 
+    for (let h = 0; h < grid.length; h++) {
+        for (let w = 0; w < grid[h].length; w++) {
+            const currentNode = nodeName(h, w);
+            graph.setNode(currentNode);
+            const currentNodeElevation = grid[h][w];
+            registerConnection(currentNode, currentNodeElevation, h, w - 1);
+            registerConnection(currentNode, currentNodeElevation, h - 1, w);
+            registerConnection(currentNode, currentNodeElevation, h, w + 1);
+            registerConnection(currentNode, currentNodeElevation, h + 1, w);
+        }
+    }
+    return graph;
+}
 
-    // TODO parse input
-    // TODO build graph
-    // TODO find shortest path
-    //
-    let graphExample = {
-        start: {A: 5, B: 2},
-        A: {start: 1, C: 4, D: 2},
-        B: {A: 8, D: 7},
-        C: {D: 6, finish: 3},
-        D: {finish: 1},
-        finish: {},
-    };
-    return input.length;
+function findFirstInGrid(grid, toBeFound) {
+    for (let h = 0; h < grid.length; h++) {
+        for (let w = 0; w < grid[h].length; w++) {
+            if (grid[h][w] === toBeFound) {
+                return {w, h, name: nodeName(h, w)};
+            }
+        }
+    }
+    return undefined;
+}
+
+function findAllInGrid(grid, toBeFound) {
+    const result = []
+    for (let h = 0; h < grid.length; h++) {
+        for (let w = 0; w < grid[h].length; w++) {
+            if (grid[h][w] === toBeFound) {
+                result.push({w, h, name: nodeName(h, w)});
+            }
+        }
+    }
+    return result;
+}
+
+function day12(input) {
+    const grid = parseMap(input);
+
+    const start = findFirstInGrid(grid, "S".charCodeAt(0));
+    grid[start.h][start.w] = "a".charCodeAt(0);
+    const end = findFirstInGrid(grid, "E".charCodeAt(0));
+    grid[end.h][end.w] = "z".charCodeAt(0);
+
+    const graph = buildGraph(grid);
+    // it could be the case graph is not connected, then it needs to be pruned, remove nodes not connected to starting node, find connected part of graph
+    // leave only vertices that are connected to
+    // const minimumSpanningTree = graphlib.alg.prim(graph, () => 1);
+    const shortestPath = graphlib.alg.dijkstra(graph, start.name);
+    return shortestPath[end.name].distance;
 }
 
 function day12Part2(input) {
-    return input.length;
+    const grid = parseMap(input);
+
+    const start = findFirstInGrid(grid, "S".charCodeAt(0));
+    grid[start.h][start.w] = "a".charCodeAt(0);
+    const end = findFirstInGrid(grid, "E".charCodeAt(0));
+    grid[end.h][end.w] = "z".charCodeAt(0);
+
+    const allAs = findAllInGrid(grid, "a".charCodeAt(0))
+
+    const graph = buildGraph(grid);
+
+    return Math.min(
+        ...allAs.map(a => {
+            const shortestPath = graphlib.alg.dijkstra(graph, a.name);
+            return shortestPath[end.name].distance
+        })
+    );
 }
 
 if (process.argv[2]) {
